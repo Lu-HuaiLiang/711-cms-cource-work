997a8a7bbcb12a04589b19d7cb6934417d3b8560
Lu-HuaiLiang <815220870@qq.com>
2021-01-12 10:01:25 +0800

    del

--- koa-server/app.js
+++
@@ -1,8 +0,0 @@
-const Koa = require('koa');
-const app = new Koa(); 
-require('module-alias/register'); // 别名插件@
-
-const InitManager = require('./core/init');
-InitManager.initCore(app);
-
-app.listen(8082)
